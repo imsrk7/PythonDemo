271089599ae471fb5eb2b8c967f1d689d0566829
imsrk7 <srk0395@gmail.com>
2023-05-11 21:46:47 +0530

    Added new file Python_Tuple and make changes in python_List

--- Python_List.py
+++ Python_List.py
@@ -1,10 +1,29 @@
 # Datatype List
+# Performing operation on List Like Create, Insert, Update, Delete, Append
+# The list is a data type that is mutable
+
 a = [1, 2, "Shubham", 10.0]
 print(a)  # Print whole List
+
 print(a[0])  # Printing '0' position element from List
+
 print(a[3])  # Print last position element form the list
+
 print(a[-1])  # Another method for getting last element form the list
+
 print(a[1:3])  # Print values in range
+
 # Inserting new element in the list
-a.insert(3, "Kondekar")
+a.insert(3, "Kondekar") # Inserting element at the 3rd position
 print(a)
+
+a.append("End")  # Added the elements at the end of the list
+print(a)
+
+a[2] = "SHUBHAM"  # used to update the value
+print(a)
+
+del a[0]  # Deleting element from the list
+print(a)
+
+print(len(a))  # printing length of list
---
+++ Python_Tuple.py
@@ -0,0 +1,8 @@
+# DataType Tuple
+# The Tuple is a data type that is immutable.
+
+values = (1, 2, "Shubham", 3.0)
+print(values)  # To Print Tuple
+print(values[1])  # To Print first position value from tuple
+print(len(values))  # Printing length of tuple
+
