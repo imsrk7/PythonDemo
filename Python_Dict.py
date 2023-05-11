88bc2fc2ba2069e3ead6d87b0448991145ff2f7b
imsrk7 <srk0395@gmail.com>
2023-05-11 23:14:41 +0530

    Studdied Python Dictonary and Tuple

---
+++ Python_Dict.py
@@ -0,0 +1,18 @@
+# DataType Dictionary
+
+a = {1: "Shubham", 2: "Kondekar", "Age": 44}
+
+print(a[1])  # Printing First value from Dictionary
+
+print(a)  # Printing Dictionary
+
+a["Middle Name"] = "ABC"
+print(a)  # Inserting new element in Dictionary
+
+# How to create dictionary at run time
+dict = {}
+
+dict[1] = "ABC"
+dict["abc"] = 2
+print(dict)
+
