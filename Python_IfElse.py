094e5b0525e7befc35ca6d3bc80a5b64f0e920e2
imsrk7 <srk0395@gmail.com>
2023-05-12 12:01:43 +0530

    Studdied IF and Else in Python

---
+++ Python_IfElse.py
@@ -0,0 +1,9 @@
+greeting = "Good Morning"
+
+if greeting == "Morning":
+    print("Condition is matched")
+    print("second Condition")
+else:
+    print("Condition is not matched")
+print("if-Else Block is executed")
+
