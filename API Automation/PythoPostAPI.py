00e3c2ac929ccac20f1579f1865acc2f963b60ce
imsrk7 <srk0395@gmail.com>
2023-05-19 00:10:11 +0530

    Python with API Automation4

--- API Automation/PythoPostAPI.py
+++ API Automation/PythoPostAPI.py
@@ -1,22 +1,24 @@
 import requests
 from payload import *
 from Utilities.configurations import *
+from Utilities.resourses import *
 
-addBook_response = requests.post(getConfig()['API']['endpoint'] + '/Library/Addbook.php',
-                                 json=addBookPayload("126HG"),
-                                 headers={'Content-Type': 'application/json'}, )
+url_addBook = getConfig()['API']['endpoint'] + APIResources.addBook
+headers = {'Content-Type': 'application/json'}
+addBook_response = requests.post(url_addBook,json=addBookPayload("128HG"),headers=headers, )
 print(addBook_response.json())
 
 response_json = addBook_response.json()
 print(type(response_json))
 bookId = response_json['ID']  # Stored ID in bookID variable
 
-response_deleteBook = requests.post('http://216.10.245.166/Library/DeleteBook.php', json={
+url_delBook = getConfig()['API']['endpoint']+ APIResources.deleteBook
+response_deleteBook = requests.post(url_delBook, json={
 
     "ID": bookId
 
 }, headers={'Content-Type': 'application/json'},
-              )
+                                    )
 assert response_deleteBook.status_code == 200
 res_json = response_deleteBook.json()
 print(res_json["msg"])
